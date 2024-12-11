ec5f1f3f26f67c5000339d0bac133e218bd3a89f
Zakariya <pzakariya@mail.ru>
2024-12-11 17:12:12 +0500

    Появился отправитель и получатель сообщений для брокера

--- SomeInteresting/interes/send.py
+++ SomeInteresting/interes/send.py
@@ -9,4 +9,5 @@ channel.basic_publish(exchange='',
                       routing_key='hello',
                       body='Hello World!')
 print(" [x] Sent 'Hello World!'")
+
 connection.close()
--- SomeInteresting/receive.py
+++ SomeInteresting/receive.py
@@ -5,14 +5,15 @@ channel = connection.channel()
 
 channel.queue_declare(queue='hello')
 
+print(' [*] Waiting for messages. To exit press CTRL+C')
+
 
 def callback(ch, method, properties, body):
-    print(f" [x] Received {body}")
+    print(f" [x] Received {body.decode('utf-8')}")
 
 
 channel.basic_consume(queue='hello',
                       on_message_callback=callback,
                       auto_ack=True)
 
-print(' [*] Waiting for messages. To exit press CTRL+C')
 channel.start_consuming()
