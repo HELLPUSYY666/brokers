555ede92a6e554dd25e67e8a25dbf9edb6370f3b
Zakariya <pzakariya@mail.ru>
2024-12-17 17:52:07 +0500

    Добавляю Postgres SQL

--- SomeInteresting/SomeInteresting/settings.py
+++ SomeInteresting/SomeInteresting/settings.py
@@ -66,14 +66,13 @@ WSGI_APPLICATION = 'SomeInteresting.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'mydatabase',
+        'NAME': 'FirstPOST',
         'USER': 'myuser',
         'PASSWORD': 'mypassword',
         'HOST': 'localhost',
         'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
